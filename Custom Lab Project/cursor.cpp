28fbb37cf21cf9d5738899339cdaf4f7b82e9ff1
Raymond <rchle001@ucr.edu>
2019-11-19 18:44:55 -0800

    commit

--- Custom Lab Project/cursor.cpp
+++ Custom Lab Project/cursor.cpp
@@ -13,13 +13,43 @@ int main()
     HWND wc;
     LPRECT rect;
     HINSTANCE hCurs1 = GetModuleHandle(NULL);
+
+    fstream file;
     
+    int x,y;
+    int Lmouse;
+    LPPOINT p;
+    unsigned short z;
     
-    int x = GetSystemMetrics(SM_CXVIRTUALSCREEN)/2;
-    int y = GetSystemMetrics(SM_CYVIRTUALSCREEN)/2;
+    GetCursorPos(p);
+
+    /*x = GetSystemMetrics(SM_CXVIRTUALSCREEN)/2;       // gets half of horizontal distance
+    y = GetSystemMetrics(SM_CYVIRTUALSCREEN)/2;*/       // gets half of vertical distance
     cout << "#" << endl;
-    SetCursorPos(x, y);
+    //SetCursorPos(x, y);
+    cout << p->x << "\t" << p->y << endl;
+    x = 765; y = 336;
     cout << "#" << endl;
+    p->x = 765; p->y = 336; // this doesn't actually work to set cursor position, just sets p's position
+    SetCursorPos(p->x, p->y); // this does though
+    // z = GetAsyncKeyState(1);
+    // cout << z << endl;
+    Lmouse = 1; // leftclick mouse solution found at https://youtu.be/j4VFo4acorQ {Simulating Mouse and Keyboard Events - C++ Game Hacking Tutorial Part 2 | Click Bots}
+    if(Lmouse)
+    {
+        INPUT input = { 0 };
+        input.type = INPUT_MOUSE;
+        input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN; // MOUSEEVENTF_{left, right, middle}DOWN
+        SendInput(1, &input, sizeof(input));
+        ZeroMemory(&input, sizeof(input));
+        input.type = INPUT_MOUSE;
+        input.mi.dwFlags = MOUSEEVENTF_MIDDLEUP; // MOUSEEVENTF_{left, right, middle}UP
+        SendInput(1, &input, sizeof(input));
+    }
+    else
+    {
+
+    }
     while(1)
     {
         
